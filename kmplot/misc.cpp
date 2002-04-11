0d8473b5d76c1fbc196d1a1e6fc97bd0edc63cb6
Matthias Messmer <matthias@familie-messmer.de>
2002-04-11 07:41:46 +0000

    fixed some compiler warnings
    svn path=/trunk/kdeedu/kmplot/; revision=148669

--- kmplot/misc.cpp
+++ kmplot/misc.cpp
@@ -47,6 +47,7 @@ XParser ps(10, 200, 20);
 void init()
 {	int ix;
 	
+        QColor tmp_color;
 	mode=ACHSEN | PFEILE | EXTRAHMEN;
 	koordx=koordy=0;
 	rsw=1.;
@@ -77,7 +78,8 @@ void init()
 	AchsenDicke=kc->readNumEntry("Achsenstärke", 5);
 	TeilstrichDicke=kc->readNumEntry("Teilstrichstärke", 3);
 	TeilstrichLaenge=kc->readNumEntry("Teilstrichlänge", 10);
-	AchsenFarbe=kc->readColorEntry("Achsenfarbe", &QColor(0, 0, 0)).rgb();
+        tmp_color.setRgb( 0, 0, 0 );
+	AchsenFarbe=kc->readColorEntry( "Achsenfarbe", &tmp_color ).rgb();
 	if(kc->readNumEntry("Beschriftung", 1)) mode|=BESCHRIFTUNG;
 	
 	// Standardwerte für das Gitter
@@ -85,22 +87,32 @@ void init()
 	kc->setGroup("Gitter");
 	GitterDicke=kc->readNumEntry("Gitterstärke", 1);
 	g_mode=kc->readNumEntry("Mode", 1);
-	GitterFarbe=kc->readColorEntry("Gitterfarbe", &QColor(192, 192, 192)).rgb();
+	tmp_color.setRgb( 192, 192, 192 );
+        GitterFarbe=kc->readColorEntry("Gitterfarbe", &tmp_color ).rgb();
 
 	// Standardwerte für die Graphen
 
 	kc->setGroup("Graphen");
 	ps.dicke0=kc->readNumEntry("Linienstärke", 5);
-	ps.fktext[0].farbe0=kc->readColorEntry("Farbe0", &QColor(255, 0, 0)).rgb();
-	ps.fktext[1].farbe0=kc->readColorEntry("Farbe1", &QColor(0, 0, 255)).rgb();
-	ps.fktext[2].farbe0=kc->readColorEntry("Farbe2", &QColor(0, 255, 0)).rgb();
-	ps.fktext[3].farbe0=kc->readColorEntry("Farbe3", &QColor(255, 0, 255)).rgb();
-	ps.fktext[4].farbe0=kc->readColorEntry("Farbe4", &QColor(255, 255, 0)).rgb();
-	ps.fktext[5].farbe0=kc->readColorEntry("Farbe5", &QColor(0, 255, 255)).rgb();
-	ps.fktext[6].farbe0=kc->readColorEntry("Farbe6", &QColor(0, 128, 0)).rgb();
-	ps.fktext[7].farbe0=kc->readColorEntry("Farbe7", &QColor(0, 0, 128)).rgb();
-	ps.fktext[8].farbe0=kc->readColorEntry("Farbe8", &QColor(0, 0, 0)).rgb();
-	ps.fktext[9].farbe0=kc->readColorEntry("Farbe9", &QColor(0, 0, 0)).rgb();
+        tmp_color.setRgb( 255, 0, 0 );
+	ps.fktext[0].farbe0=kc->readColorEntry("Farbe0", &tmp_color ).rgb(); 
+        tmp_color.setRgb( 0, 255, 0 );
+        ps.fktext[1].farbe0=kc->readColorEntry("Farbe1", &tmp_color ).rgb();
+        tmp_color.setRgb( 0, 0, 255 );
+	ps.fktext[2].farbe0=kc->readColorEntry("Farbe2", &tmp_color ).rgb(); 
+        tmp_color.setRgb( 255, 0, 255 );
+        ps.fktext[3].farbe0=kc->readColorEntry("Farbe3", &tmp_color ).rgb();
+        tmp_color.setRgb( 255, 255, 0 );
+	ps.fktext[4].farbe0=kc->readColorEntry("Farbe4", &tmp_color ).rgb(); 
+        tmp_color.setRgb( 0, 255, 255 );
+        ps.fktext[5].farbe0=kc->readColorEntry("Farbe5", &tmp_color ).rgb();
+        tmp_color.setRgb( 0, 128, 0 );
+	ps.fktext[6].farbe0=kc->readColorEntry("Farbe6", &tmp_color ).rgb(); 
+        tmp_color.setRgb( 0, 0, 128 );
+        ps.fktext[7].farbe0=kc->readColorEntry("Farbe7", &tmp_color ).rgb(); 
+        tmp_color.setRgb( 0, 0, 0 );
+        ps.fktext[8].farbe0=kc->readColorEntry("Farbe8", &tmp_color ).rgb(); 
+        ps.fktext[9].farbe0=kc->readColorEntry("Farbe9", &tmp_color ).rgb();
 
 	for(ix=0; ix<ps.ufanz; ++ix) ps.delfkt(ix);
 }

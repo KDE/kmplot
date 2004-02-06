fa4f7d0b47fa20a34f23557fe48f96bfd8214e05
Anne-Marie Mahfouf <annma@kde.org>
2004-02-06 18:10:42 +0000

    2 patches from Albert Astals Cid, thanks a lot CCMAIL: 72094-done@bugs.kde.org
    svn path=/trunk/kdeedu/kmplot/; revision=285606

--- kmplot/main.cpp
+++ kmplot/main.cpp
@@ -47,11 +47,11 @@ int main( int argc, char **argv )
 	    "kmplot",
 	    I18N_NOOP( "KmPlot" ),
 	    KP_VERSION, description, KAboutData::License_GPL,
-	    "(c) 2000-2002, Klaus-Dieter Möller",
+	    "(c) 2000-2002, Klaus-Dieter MÃ¶ller",
 	    0, 0,
 	    "submit@bugs.kde.org" );
 	aboutData.addAuthor(
-	    "Klaus-Dieter Möller", 0 ,
+	    "Klaus-Dieter MÃ¶ller", 0 ,
 	    "kd.moeller@t-online.de" );
 	aboutData.addCredit("David Vignoni", I18N_NOOP("svg icon"), "david80v@tin.it");
 	KCmdLineArgs::init( argc, argv, &aboutData );

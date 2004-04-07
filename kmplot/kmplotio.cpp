cec86bc03e72f2f3dea0c6527fde78be14b26c90
Matthias Messmer <matthias@familie-messmer.de>
2004-04-07 18:22:06 +0000

    fixed the header -> GPL
    svn path=/trunk/kdeedu/kmplot/; revision=302006

--- kmplot/kmplotio.cpp
+++ kmplot/kmplotio.cpp
@@ -1,14 +1,27 @@
-//
-// C++ Implementation: kmplotio
-//
-// Description: 
-//
-//
-// Author: Klaus-Dieter Moeller <kd.moeller@t-online.de>, (C) 2004
-//
-// Copyright: See COPYING file that comes with this distribution
-//
-//
+/*
+* KmPlot - a math. function plotter for the KDE-Desktop
+*
+* Copyright (C) 1998, 1999  Klaus-Dieter Möller
+*               2000, 2002 kd.moeller@t-online.de
+*
+* This file is part of the KDE Project.
+* KmPlot is part of the KDE-EDU Project.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*
+*/
 
 // Qt includes
 #include <qdom.h>

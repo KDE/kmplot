9393a8c5b6891f3635b68a2275ed0c55376e0149
Stephan Kulow <coolo@kde.org>
2006-05-01 19:08:32 +0000

    moved messages targets out of Makefile.am
    svn path=/trunk/KDE/kdeedu/kmplot/; revision=536286

---
+++ kmplot/Messages.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+$EXTRACTRC *.ui >> rc.cpp || exit 11
+LIST=`find . -name \*.h -o -name \*.hh -o -name \*.H -o -name \*.hxx -o -name \*.hpp -o -name \*.cpp -o -name \*.cc -o -name \*.cxx -o -name \*.ecpp -o -name \*.C`
+if test -n "$LIST"; then
+$XGETTEXT $LIST rc.cpp -o $podir/kmplot.pot
+fi

ad2d0ebb4d854b54fee8cd2ddf2ccead13c858e0
Stephan Kulow <coolo@kde.org>
2006-07-03 14:59:03 +0000

    compile
    svn path=/trunk/KDE/kdeedu/kmplot/; revision=557565

--- kmplot/kmplotadaptor.h
+++ kmplot/kmplotadaptor.h
@@ -11,7 +11,7 @@
 #define KMPLOTADAPTOR_H_251211149672234
 
 #include <QtCore/QObject>
-#include <dbus/qdbus.h>
+#include <QtDBus/qdbus.h>
 class QByteArray;
 template<class T> class QList;
 template<class Key, class Value> class QMap;

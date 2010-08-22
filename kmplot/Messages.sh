07eb9f7ba2e23c9622011db2a136d5f9be7a6cc6
Albert Astals Cid <tsdgeos@terra.es>
2010-08-22 13:48:28 +0000

    fix Messages.sh after yesterday change in extract-messages.sh
    svn path=/trunk/KDE/kdeedu/kmplot/; revision=1166698

--- kmplot/Messages.sh
+++ kmplot/Messages.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-$EXTRACTRC *.ui >> rc.cpp || exit 11
+$EXTRACTRC *.ui *.kcfg *.rc >> rc.cpp || exit 11
 LIST=`find . -name \*.h -o -name \*.hh -o -name \*.H -o -name \*.hxx -o -name \*.hpp -o -name \*.cpp -o -name \*.cc -o -name \*.cxx -o -name \*.ecpp -o -name \*.C`
 if test -n "$LIST"; then
 $XGETTEXT $LIST rc.cpp -o $podir/kmplot.pot
